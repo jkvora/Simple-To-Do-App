d566c8ae2402863c8546852d629e94afffd0e51a
Jalak <vorajalak@yahoo.co.in>
2016-01-26 14:18:28 +0530

    Adding list and simply displaying them

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,22 @@ var app=angular.module('myApp',[]);
 
 app.controller('appcontroller',function($scope){
 
+$scope.taskslist=[];
+
+$scope.pressEnter=function() {
+  if(event.which==13){
+    $scope.addTask();
+    $scope.list="";
+  }
+};
+
+$scope.addTask=function () {
+  if($scope.list!=""){
+    $scope.taskslist.push($scope.list);
+  }
+    console.log($scope.taskslist);
+};
+
+
+
 });
